93c94df3bd3e4193b3911e563a4d885272e5fcd7
ccconac <qwerpoiu91@naver.com>
2025-05-20 17:06:59 +0900

    feat(코딩 테스트 합격자 되기): 몸풀기 문제 08 괄호 짝 맞추기

---
+++ 코딩 테스트 합격자 되기/6장 스택/08.js
@@ -0,0 +1,19 @@
+function solution(str) {
+  const stack = [];
+
+  // 1. 소괄호가 정상적으로 열고 닫혔다면 true 반환
+  // 2. 정상적으로 닫히지 않았다면 false 반환
+  for (const value of str) {
+    if (value === '(') stack.push(value);
+    else if (value === ')') {
+      if (!stack.length) return false;
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(solution('(())()'));
+console.log(solution('((())()'));
+console.log(solution('((())))))))))()'));
