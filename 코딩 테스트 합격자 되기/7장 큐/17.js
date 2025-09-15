dc823bb71f1a059311ae3c58b8769146fddd52a7
ccconac <qwerpoiu91@naver.com>
2025-09-15 16:09:22 +0900

    feat(코딩 테스트 합격자 되기): 모의 테스트 17번 카드 뭉치

---
+++ 코딩 테스트 합격자 되기/7장 큐/17.js
@@ -0,0 +1,28 @@
+function solution(cards1, cards2, goal) {
+  let j = 0;
+  let k = 0;
+
+  for (let i = 0; i < goal.length; i++) {
+    if (cards1[j] === goal[i]) j++;
+    else if (cards2[k] === goal[i]) k++;
+    else return 'No';
+  }
+
+  return 'Yes';
+}
+
+console.log(
+  solution(
+    ['i', 'drink', 'water'],
+    ['want', 'to'],
+    ['i', 'want', 'to', 'drink', 'water']
+  )
+);
+
+console.log(
+  solution(
+    ['i', 'water', 'drink'],
+    ['want', 'to'],
+    ['i', 'want', 'to', 'drink', 'water']
+  )
+);
